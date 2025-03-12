18ba9fbf6c6cf1cc8f3b144b81b334d44094d8ef
CodingKitten <121097932+CodingKitten-YT@users.noreply.github.com>
2025-03-12 17:27:27 +0000

    Wow

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+export default defineConfig(({ command }) => {
+  const isProduction = command === 'build';
+  
+  return {
+    base: isProduction ? '/KittenFlix/' : '/',
+    plugins: [react()],
+    optimizeDeps: {
+      exclude: ['lucide-react'],
+    },
+  };
 });
