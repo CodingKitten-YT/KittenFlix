4e85ce8510cd5a723b7c3f18f296b5d92dbddd66
CodingKitten <121097932+CodingKitten-YT@users.noreply.github.com>
2025-03-31 18:52:52 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,18 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Use root path for all environments
+  base: '/movies/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    host: true,
+    port: 4000,
+  },
+  preview: {
+    host: true,
+    port: 4000,
+    allowedHosts: true
+  }
 });
